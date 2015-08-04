7e7e4fb858ec94b704b960d545684b0e25718be4
Joshua Borton <digitaltoad@gmail.com>
2015-08-04 01:55:15 -0400

    Add default_namespace and default_postfix

--- lib/active_model/serializer/configuration.rb
+++ lib/active_model/serializer/configuration.rb
@@ -7,6 +7,8 @@ module ActiveModel
       included do |base|
         base.config.array_serializer = ActiveModel::Serializer::ArraySerializer
         base.config.adapter = :flatten_json
+        base.config.default_namespace = ""
+        base.config.default_postfix = "Serializer"
       end
     end
   end
