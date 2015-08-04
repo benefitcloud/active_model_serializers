7b37f5ec002ba06f862492cbfde4598bafadd024
Joshua Borton <digitaltoad@gmail.com>
2015-08-04 01:53:53 -0400

    Only load railtie if rails is found

--- lib/active_model/serializer/railtie.rb
+++ lib/active_model/serializer/railtie.rb
@@ -1,9 +1,11 @@
-require 'rails/railtie'
-module ActiveModel
-  class Railtie < Rails::Railtie
-    initializer 'generators' do |app|
-      app.load_generators
-      require 'generators/serializer/resource_override'
+begin
+  require 'rails/railtie'
+  module ActiveModel
+    class Railtie < Rails::Railtie
+      initializer 'generators' do |app|
+        app.load_generators
+        require 'generators/serializer/resource_override'
+      end
     end
   end
-end
+rescue LoadError; end
